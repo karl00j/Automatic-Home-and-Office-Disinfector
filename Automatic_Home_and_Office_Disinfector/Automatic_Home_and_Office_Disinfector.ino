8b9346a6e8c2cbbd03d5a876605e1366ca78312e
karl00j <82194655+karl00j@users.noreply.github.com>
2021-06-18 23:59:21 +0800

    Add files via upload

---
+++ Automatic_Home_and_Office_Disinfector/Automatic_Home_and_Office_Disinfector.ino
@@ -0,0 +1,159 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(1, 2, 3, 4, 5, 6);
+
+//for first Ultrasonic sensor
+const int trigPin = 8;
+const int echoPin = 9;
+
+//for second Ultrasonic sensor
+const int trigPin1 = 10;
+const int echoPin1 = 11;
+
+//for addition button
+const int pushButton = 22;
+int buttonRead = 0;
+
+//for subtraction button
+const int pushButton1 = 24;
+int buttonRead1 = 0;
+
+//for buzzer
+const int buzzer = 26;
+
+const int led = 30;
+const int led1 = 32;
+const int output = 52;
+
+
+
+
+class RoomDisinfector{
+
+  public:
+
+    int incomingGuests(int count){
+      int duration, distance;
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      
+      duration = pulseIn(echoPin, HIGH);
+      distance = (duration*0.034)/2;
+
+      if(distance <= 12){
+        count = count + 1;
+        digitalWrite(led, HIGH);
+        delay(500);
+        digitalWrite(led, LOW);
+      }
+      return count;
+    }
+
+    int outgoingGuests(){
+      int duration1, distance1;
+      int val=0;
+      digitalWrite(trigPin1, LOW);
+      delayMicroseconds(2);
+      
+      digitalWrite(trigPin1, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin1, LOW);
+
+      duration1 = pulseIn(echoPin1, HIGH);
+      distance1 = (duration1*0.034)/2;
+
+      if(distance1 <= 12){
+          val = val+1;
+          digitalWrite(led1, HIGH);
+          delay(500);
+          digitalWrite(led1, LOW);
+        }
+     
+      buttonRead = digitalRead(pushButton);
+      buttonRead1 = digitalRead(pushButton1);
+
+      if (buttonRead == HIGH){
+       val = val + 1;
+       digitalWrite(led1, HIGH);
+       delay(500);
+       digitalWrite(led1, LOW);
+      }
+
+      if(buttonRead1 == HIGH){
+       val = val-1;
+       digitalWrite(led1, HIGH);
+       delay(500);
+       digitalWrite(led1, LOW);
+      }
+      return val;
+    }
+
+    void alarm(){
+      tone(buzzer, 450);
+      delay(3000);
+      noTone(buzzer);
+      
+    }
+
+    void lcdDisplay(int passedVal, int passedVal1){
+     Serial.print("Incoming: ");
+     Serial.println(passedVal);
+
+     Serial.print("Outgoing: ");
+     Serial.println(passedVal1);
+     
+     lcd.setCursor(12, 0);
+     lcd.print(passedVal);
+     lcd.setCursor(10, 1);
+     lcd.print(passedVal1);
+     
+     
+     
+     }
+  
+
+
+  
+};
+
+
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(pushButton, INPUT);
+  pinMode(pushButton1, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Population: ");
+  lcd.setCursor(0, 1);
+  lcd.print("Outgoing: ");
+}
+
+void loop() {
+  int count = 0;
+  RoomDisinfector r;
+  
+  int temp, temp1;
+  temp = r.incomingGuests(count);
+  temp1 = r.outgoingGuests();
+
+  r.lcdDisplay(temp, temp1);
+  
+  if(temp <= temp1){
+    r.alarm();
+    delay(300000);
+    digitalWrite(output, HIGH);
+  }
+}
